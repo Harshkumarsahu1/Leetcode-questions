fe60357f5aa871c829378b8774601ebbb453fef8
Harsh Kumar Sahu <104484483+Harshkumarsahu1@users.noreply.github.com>
2024-06-28 10:08:26 +0530

    Time: 265 ms (23.77%), Space: 57.5 MB (5.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,62 @@
 
 class Solution{
-    String longestPalindrome(String S){
+    // String longestPalindrome(String S){
         
-        int curr =0;
+    //     int curr =0;
+    //     int n = S.length();
+    //     //Character [] ch = S.split("");
+    //     int left = 0;
+    //     int right = S.length()-1;
+    //     String answer = "";
+       
+    //         if(n==1){
+    //             return S;
+    //         }
+            
+            
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             if(isPalindrome(S.substring(i,j+1))){
+    //                 if(answer.length()<S.substring(i,j+1).length()){
+    //                     answer = S.substring(i,j+1);
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return answer;
+    // }
+    
+    // boolean isPalindrome(String S){
+    //     int left  = 0;
+    //     int right = S.length()-1;
+        
+    //     while(left<=right){
+    //         if(S.charAt(left)!=S.charAt(right)){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
+    Boolean dp[][];
+    
+    String longestPalindrome(String S) {
         int n = S.length();
-        //Character [] ch = S.split("");
-        int left = 0;
-        int right = S.length()-1;
+        if (n == 1) {
+            return S;
+        }
+        
+        dp = new Boolean[n][n];
         String answer = "";
-        // while(left<=right){
-        //     if(ch[left]==ch[right]){
-        //         int size=(right-left)+1;
-        //         if(size>cur){
-        //             ans=S.subString(left,right+1);
-        //         }
-        //         left++;
-        //         right --;
-        //     }
-        //     else{
-        //         curr=left;
-        //         right=n-1;
-        //     }
-            
-            
-        // }
-            if(n==1){
-                return S;
-            }
-            
-            
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(S.substring(i,j+1))){
-                    if(answer.length()<S.substring(i,j+1).length()){
-                        answer = S.substring(i,j+1);
+        
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (isPalindrome(S, i, j)) {
+                    if (len > answer.length()) {
+                        answer = S.substring(i, j + 1);
                     }
                 }
             }
@@ -42,18 +65,17 @@ class Solution{
         return answer;
     }
     
-    boolean isPalindrome(String S){
-        int left  = 0;
-        int right = S.length()-1;
-        
-        while(left<=right){
-            if(S.charAt(left)!=S.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+    boolean isPalindrome(String s, int left, int right) {
+        if (left >= right) {
+            return true;
         }
-        return true;
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+        if (s.charAt(left) != s.charAt(right)) {
+            return dp[left][right] = false;
+        }
+        return dp[left][right] = isPalindrome(s, left + 1, right - 1);
     }
     
 }
