cb8711a7031a367a086b71ec7f6a34d1089ec17f
Harsh Kumar Sahu <104484483+Harshkumarsahu1@users.noreply.github.com>
2024-06-28 09:47:06 +0530

    Time: 27 ms (27.15%), Space: 48.7 MB (9.18%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,7 +1,9 @@
 
 class Solution{
+    int memo[][];
     int countSubstrings(String S){
-        
+        memo=new int[S.length()+1][S.length()+1];
+        for(int row[]:memo) Arrays.fill(row,-1);
         int curr =0;
         int n = S.length();
         //Character [] ch = S.split("");
@@ -9,22 +11,7 @@ class Solution{
         int right = S.length()-1;
         String answer = "";
         int count = 0;
-        // while(left<=right){
-        //     if(ch[left]==ch[right]){
-        //         int size=(right-left)+1;
-        //         if(size>cur){
-        //             ans=S.subString(left,right+1);
-        //         }
-        //         left++;
-        //         right --;
-        //     }
-        //     else{
-        //         curr=left;
-        //         right=n-1;
-        //     }
-            
-            
-        // }
+    
             if(n==1){
                 return 1;
             }
@@ -32,11 +19,7 @@ class Solution{
             
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPalindrome(S.substring(i,j+1))){
-                    // if(answer.length()<S.substring(i,j+1).length()){
-                    //     //answer = S.substring(i,j+1);
-                        
-                    // }
+                if(isPalindrome(S,i,j)==1){ 
                     count ++;
                 }
             }
@@ -45,17 +28,29 @@ class Solution{
         return count;
     }
     
-    boolean isPalindrome(String S){
-        int left  = 0;
-        int right = S.length()-1;
+    // boolean isPalindrome(String S){
+    //     int left  = 0;
+    //     int right = S.length()-1;
         
-        while(left<=right){
-            if(S.charAt(left)!=S.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+    //     while(left<=right){
+    //         if(S.charAt(left)!=S.charAt(right)){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
+     int isPalindrome(String s, int i, int j) {
+        if (i >= j) {
+            return 1;
         }
-        return true;
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        if (s.charAt(i) != s.charAt(j)) {
+            return memo[i][j] = 0;
+        }
+        return memo[i][j] = isPalindrome(s, i + 1, j - 1);
     }
     }
